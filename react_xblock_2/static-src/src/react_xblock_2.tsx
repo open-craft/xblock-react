892db7ea5e24ff85caba9392c6095a5e58e54168
Braden MacDonald <braden@opencraft.com>
2024-12-05 11:12:39 -0800

    fix: remove Paragon as it's broken and its SCSS is not present anyways

--- react_xblock_2/static-src/src/react_xblock_2.tsx
+++ react_xblock_2/static-src/src/react_xblock_2.tsx
@@ -3,14 +3,7 @@
 // npm run build       or      npm run watch                                  //
 ////////////////////////////////////////////////////////////////////////////////
 import React from 'react';
-import ReactDOM from 'react-dom';
-// 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑
-// 🛑 WARNING: Paragon will NOT work at the moment due to including .scss,    🛑
-// 🛑 .svg, and .png imports in its "dist" build :/ You need to either modify 🛑
-// 🛑 it in node_modules/@openedx/paragon/dist/index.js (comment out every    🛑
-// 🛑 line below "Things that don't have types"), or don't use Paragon.       🛑
-// 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑
-import { Button } from '@openedx/paragon';
+import ReactDOM from 'react-dom/client';
 import { BoundRuntime, type JQueryWrappedDiv, type XBlockRuntime } from './xblock-utils';
 
 /** Data passed from our student_view render method when it calls frag.initialize_js() */
@@ -37,7 +30,7 @@ const StudentView: React.FC<Props> = ({ runtime, ...props }) => {
 
   return <div className="react_xblock_2_block">
       <p>ReactXBlock8: count is now <span className="count">{count}</span>.</p>
-      <Button onClick={increment}>+ Increment</Button>
+      <button className="btn btn-primary" onClick={increment}>+ Increment</button>
     </div>
 }
 
@@ -46,14 +39,8 @@ function initStudentView(runtime: XBlockRuntime, container: HTMLDivElement | JQu
     // Fix inconsistent parameter typing:
     container = container[0];
   }
-  // Paragon doesn't yet support React 18:
-  //   const root = createRoot(container!);
-  //   root.render(<StudentView runtime={new BoundRuntime(runtime, container)} initialCount={initData.count} />);
-  // So use the deprecated React 17 API:
-  ReactDOM.render(
-    <StudentView runtime={new BoundRuntime(runtime, container)} initialCount={initData.count} />,
-    container
-  );
+  const root = ReactDOM.createRoot(container!);
+  root.render(<StudentView runtime={new BoundRuntime(runtime, container)} initialCount={initData.count} />);
 }
 
 // Thanks to rollup's output.name setting, this will become 'initReactXBlock8StudentView' in the global scope
